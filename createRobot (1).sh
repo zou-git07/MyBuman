84ce1c46b85fd0ebf792b0cb8e0fae11b4a26269
stars206826 <wangerwang008@gmail.com>
2025-12-22 15:56:35 +0800

    首次提交代码

---
+++ createRobot (1).sh
@@ -0,0 +1,30 @@
+#run this script in root directory of coderelease 
+sudo Install/createRobot -t 70 -r 1 -s P0000074A05S92L00037 -b P0000073A07S93M00018 -l 192.168 -w 10.0 fly1
+
+sudo Install/createRobot -t 70 -r 3 -s P0000074A03S86800024 -b P0000073A07S93M00043 -l 192.168 -w 10.0 fly3
+
+sudo Install/createRobot -t 70 -r 4 -s P0000074A05S93M00025 -b P0000073A07S92D00039 -l 192.168 -w 10.0 fly4
+
+sudo Install/createRobot -t 70 -r 5 -s P0000074A05S92D00015 -b P0000073A07S92D00049 -l 192.168 -w 10.0 fly5
+
+sudo Install/createRobot -t 70 -r 6 -s P0000074A05S92D00040 -b P0000073A07S92D00041 -l 192.168 -w 10.0 fly6
+
+sudo Install/createRobot -t 70 -r 7 -s P0000074A05S92D00012 -b P0000073A07S92D00053 -l 192.168 -w 10.0 fly7
+
+sudo Install/createRobot -t 70 -r 8 -s P0000074A05S92D00052 -b P0000073A07S92L00032 -l 192.168 -w 10.0 fly8
+
+sudo Install/createRobot -t 70 -r 11 -s P0000074A10S49K00002 -b P0000073A20S49K00002 -l 192.168 -w 10.0 fly11
+
+sudo Install/createRobot -t 70 -r 12 -s P0000074A09S36Q00025 -b P0000073A19S36Q00028 -l 192.168 -w 10.0 fly12
+
+sudo Install/createRobot -t 70 -r 13 -s P0000074A10S45K00005 -b P0000073A20S45K00004 -l 192.168 -w 10.0 fly13
+
+sudo Install/createRobot -t 70 -r 14 -s P0000074A10S45K00003 -b P0000073A20S45K00002 -l 192.168 -w 10.0 fly14
+
+sudo Install/createRobot -t 70 -r 15 -s P0000074A10S49400027 -b P0000073A20S49400027 -l 192.168 -w 10.0 fly15
+
+sudo Install/createRobot -t 70 -r 21 -s P0000074A04S91K00007 -b P0000073A06S91K00002 -l 192.168 -w 10.0 nb21
+
+sudo Install/createRobot -t 70 -r 22 -s P0000074A04S8AJ00007 -b P0000073A06S8AJ00002 -l 192.168 -w 10.0 nb22
+
+sudo Install/createRobot -t 70 -r 23 -s P0000074A04S91900060 -b P0000073A06S91900060 -l 192.168 -w 10.0 nb23
